b73cf70b07794232e39bd320465794abb3522d68
braxtondcoleman <braxton.d.coleman@gmail.com>
2026-06-23 17:20:27 -0600

    Working countdown prototype

--- src/main/java/com/example/examplemod/client/gui/CountdownScreen.java
+++ src/main/java/com/example/examplemod/client/gui/CountdownScreen.java
@@ -5,7 +5,8 @@ import net.minecraft.network.chat.Component;
 
 public class CountdownScreen extends Screen {
 
-    private int countdownTicks = 60; // 3 seconds at 20 TPS
+    private int countdownTicks = 60;
+    private int lastSecondShown = 3;
 
     public CountdownScreen() {
         super(Component.literal("Traveling"));
@@ -15,6 +16,18 @@ public class CountdownScreen extends Screen {
     public void tick() {
 
         countdownTicks--;
+
+        int secondsRemaining = (countdownTicks + 19) / 20;
+
+        if (secondsRemaining != lastSecondShown) {
+            lastSecondShown = secondsRemaining;
+
+            if (minecraft != null && minecraft.player != null && secondsRemaining > 0) {
+                minecraft.player.sendSystemMessage(
+                    Component.literal("Traveling in " + secondsRemaining + "...")
+                );
+            }
+        }
 
         if (countdownTicks <= 0) {
 
@@ -26,5 +39,17 @@ public class CountdownScreen extends Screen {
 
             minecraft.setScreen(null);
         }
+    }
+
+    @Override
+    public void onClose() {
+
+        if (minecraft != null && minecraft.player != null) {
+            minecraft.player.sendSystemMessage(
+                Component.literal("Travel Cancelled")
+            );
+        }
+
+        minecraft.setScreen(null);
     }
 }
