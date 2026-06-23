9602c8686e20cf4d676469ddedca11649af97bd8
braxtondcoleman <braxton.d.coleman@gmail.com>
2026-06-23 16:29:27 -0600

    Add H keybind and chat message

--- src/main/java/com/example/examplemod/CrossroadDimensionClient.java
+++ src/main/java/com/example/examplemod/CrossroadDimensionClient.java
@@ -10,6 +10,9 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import com.example.examplemod.client.KeybindHandler;
+
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = CrossroadDimension.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
@@ -27,5 +30,10 @@ public class CrossroadDimensionClient {
         // Some client setup code
         CrossroadDimension.LOGGER.info("HELLO FROM CLIENT SETUP");
         CrossroadDimension.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+
+    @SubscribeEvent
+    public static void registerKeys(RegisterKeyMappingsEvent event) {
+        event.register(KeybindHandler.OPEN_CROSSROADS);
     }
 }
---
+++ src/main/java/com/example/examplemod/client/KeybindHandler.java
@@ -0,0 +1,40 @@
+package com.example.examplemod.client;
+
+import com.mojang.blaze3d.platform.InputConstants;
+
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
+@EventBusSubscriber
+public class KeybindHandler {
+
+    public static final KeyMapping OPEN_CROSSROADS =
+        new KeyMapping(
+                "key.crossroaddimension.open",
+                KeyConflictContext.IN_GAME,
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_H,
+                KeyMapping.Category.MISC
+        );
+
+    @SubscribeEvent
+    public static void onClientTick(ClientTickEvent.Post event) {
+
+        while (OPEN_CROSSROADS.consumeClick()) {
+
+            Minecraft mc = Minecraft.getInstance();
+
+            if (mc.player != null) {
+                mc.player.sendSystemMessage(
+                        Component.literal("Opening Crossroads...")
+                );
+            }
+        }
+    }
+}
